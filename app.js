dcebdc0c8c97e026ea4f80b41911e156f6eeb566
Mactavish410 <Mactavish410@gmail.com>
2023-02-17 21:43:05 +0500

    my-work

---
+++ app.js
@@ -0,0 +1,27 @@
+$(function () {
+  $(".my-skills__icon").mouseover(function(){
+     this.children[0].src = `/img/icon;hover/icon ${this.children[0].alt} -- hover.svg`  
+   });
+   
+    $(".my-skills__icon").mouseout(function(){
+        this.children[0].src = `/img/icon/icon ${this.children[0].alt}.svg`
+   });
+});
+
+
+$(".my-work__block").click(function(event){
+     console.log(this)
+     let active =  this.children[0]
+     active.classList.toggle('my-work__img-active')
+   });
+// $(function () {
+//      $(".my-work__block").mouseover(function(){
+//          let active =  this.children[0]
+//          active.classList.toggle('my-work__img-active')
+//       });
+      
+//        $(".my-skills__icon").mouseout(function(){
+//           // let active =  this.children[0]
+//           // active.classList.toggle('my-work__img-active')
+//       });
+//    });
