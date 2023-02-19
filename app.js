d5068dd6c42a7edbfaa72772d01791ece910b1fa
Mactavish410 <Mactavish410@gmail.com>
2023-02-19 15:46:19 +0500

    footer

--- app.js
+++ app.js
@@ -8,20 +8,26 @@ $(function () {
    });
 });
 
-
-$(".my-work__block").click(function(event){
-     console.log(this)
-     let active =  this.children[0]
-     active.classList.toggle('my-work__img-active')
+$(function () {
+  $(".my-work__block").mouseover(function(){
+      let active =  this.children[0]
+      active.classList.toggle('my-work__img-active')
    });
-// $(function () {
-//      $(".my-work__block").mouseover(function(){
-//          let active =  this.children[0]
-//          active.classList.toggle('my-work__img-active')
-//       });
-      
-//        $(".my-skills__icon").mouseout(function(){
-//           // let active =  this.children[0]
-//           // active.classList.toggle('my-work__img-active')
-//       });
+   
+    $(".my-work__block").mouseout(function(){
+      let active =  this.children[0]
+      active.classList.toggle('my-work__img-active')
+   });
+});
+
+// $(".my-work__block").click(function(event){
+//      console.log(this)
+//      let active =  this.children[0]
+//      active.classList.toggle('my-work__img-active')
+//    });
+
+// $(".my-work__block").click(function(event){
+//      console.log(this)
+//      let active =  this.children[0]
+//      active.classList.toggle('my-work__img-active')
 //    });
